b2b530a120cdf5c86ccb3dfff6ac8c69a87add6d
Joe <jeguchi11@gmail.com>
2025-09-21 09:55:15 +0900

    kadai_mdn_jsの追加

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,10 @@
+
+
+const date = new Date();
+let FullYear0 = date.getFullYear();
+let Month0 = date.getMonth();
+let Date0 = date.getDate();
+let Hours0 = date.getHours();
+let Minutes0 = date.getMinutes();
+
+console.log(FullYear0*10000+Month0*100+Date0);
